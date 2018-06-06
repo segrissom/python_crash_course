c03a2d16839134d055fc64495d88ad8e18561a2b
Sarah <segrissom@randolphcollege.edu>
2018-06-06 17:06:19 -0500

    PCC Chapter 6 exercise 2
    From Python Crash Course Chapter 6, exercise 2

---
+++ pcc_ex6-2.py
@@ -0,0 +1,13 @@
+#This is from the Python Crash Course book, Chapter 6 exercise 2
+favorite_numbers = {
+    'Sally': 2,
+    'Will': 4,
+    'Tim': 22,
+    'Jimothy': 909009,
+    'Simothy': 0
+}
+print("Sally's favorite number is " + favorite_numbers['Sally']+ ".")
+print("Will's favorite number is " + favorite_numbers['Will']+ ".")
+print("Tim's favorite number is " + favorite_numbers['Tim']+ ".")
+print("Jimothy's favorite number is " + favorite_numbers['Jimothy']+ ".")
+print("Simothy's favorite number is " + favorite_numbers['Simothy']+ ".")
