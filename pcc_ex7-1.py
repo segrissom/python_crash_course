795b2ae264290cb0d8252de4a11279173bfecb1f
Sarah <segrissom@randolphcollege.edu>
2018-06-07 14:58:57 -0500

    Python Crash Course Chapt 7, Exercise 2
    From the book Python Crash Course

---
+++ pcc_ex7-1.py
@@ -0,0 +1,4 @@
+#This is for Python Crash Course Chapter 7's exercise 1
+message=("Please enter the type of car you would like to rent: ")
+car_type=input(message)
+print("Let my see if I can find you a " + car_type + ".")
